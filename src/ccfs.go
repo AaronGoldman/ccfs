87d54a54113c3c56b100b5d942d7fed07514218b
Aaron Goldman <GoldmanAaron@gmail.com>
2013-03-17 01:05:20 -0400

    stubed post object functions

--- src/ccfs.go
+++ src/ccfs.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"encoding/pem"
-	"errors"
+	//	"errors"
 	"fmt"
 	"io/ioutil"
+	"path/filepath"
 )
 
 func main() {
@@ -12,27 +13,44 @@ func main() {
 	//commitgentest()
 	//taggentest()
 	//hashfindwalk()
-
+	return
 }
 
 func getBlob(hash [32]byte) (data []byte, err error) {
 	//Validate input
 	filepath := fmt.Sprintf("../blobs/%s", hash)
-	data, err := ioutil.ReadFile(filepath)
+	data, err = ioutil.ReadFile(filepath)
 	return
 }
 
-func getTag(hkid [32]byte , nameSegment string) (data []byte, err error) {
+func postBlob(data []byte) (err error) {
+	return
+}
+
+func getTag(hkid [32]byte, nameSegment string) (data []byte, err error) {
 	//Validate input
-	filepath := fmt.Sprintf("../tags/%s/%s/%s",hkid,nameSegment,version)
-	data, err := ioutil.ReadFile(filepath)
+	//matches []string, err error
+	matches, err := filepath.Glob(fmt.Sprintf("../tags/%s/%s/*", hkid, nameSegment))
+	version := latestVersion(matches)
+	filepath := fmt.Sprintf("../tags/%s/%s/%s", hkid, nameSegment, version)
+	data, err = ioutil.ReadFile(filepath)
+	return
+}
+
+func postTag(data []byte, nameSegment string, version string) (err error) {
 	return
 }
 
 func getCommit(hkid [32]byte) (data []byte, err error) {
 	//Validate input
-	filepath := fmt.Sprintf("../commits/%s/%s",hkid,version)
-	data, err := ioutil.ReadFile(filepath)
+	matches, err := filepath.Glob(fmt.Sprintf("../commits/%s/*", hkid))
+	version := latestVersion(matches)
+	filepath := fmt.Sprintf("../commits/%s/%s", hkid, version)
+	data, err = ioutil.ReadFile(filepath)
+	return
+}
+
+func postCommit(data []byte, version string) (err error) {
 	return
 }
 
@@ -44,6 +62,20 @@ func getKey(hkid [32]byte) (data []byte, err error) {
 		data = dataBlock.Bytes
 	} else {
 		data = []byte("")
+	}
+	return
+}
+
+func postKey(data []byte) (err error) {
+	return
+}
+
+func latestVersion(matches []string) (match string) {
+	match = ""
+	for _, element := range matches {
+		if match < element {
+			match = element
+		}
 	}
 	return
 }
