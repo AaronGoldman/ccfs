0810f385cf81d976626ce6539274542b887c1fa5
Aaron Goldman <GoldmanAaron@gmail.com>
2013-05-30 01:35:50 -0400

    uptades to post sill not fuctional

--- src/ccfs.go
+++ src/ccfs.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	go BlobServerStart()
+	//go BlobServerStart()
 	//hashfindwalk()
 	in := bufio.NewReader(os.Stdin)
 	input, err := in.ReadString('\n')
@@ -25,6 +25,7 @@ func get(hkid []byte, path string) (b blob, err error) {
 	objecthash := hkid
 	err = nil
 	nameSegments := []string{"", path}
+
 	for {
 		if typeString == "blob" || nameSegments[1] == "" {
 			b, err = GetBlob(objecthash)
@@ -67,33 +68,51 @@ func post(hkid []byte, path string, b blob) (err error) {
 	//isue the blob regen list isue list regen tag/commit isue tag/commit
 	typeString := "commit"
 	objecthash := hkid
+	regenlist := [][]byte{}
+	regenpath := []string{}
 	err = nil
 	nameSegments := []string{"", path}
 	for {
 		if typeString == "blob" || nameSegments[1] == "" {
 			b, err = GetBlob(objecthash)
+			if err == nil {
+
+			}
+			//if pulled blob maches no update nesisary
 		}
 		if typeString == "list" {
 			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
-			l, _ := GetList(objecthash)
+			l, err := GetList(objecthash)
+			if err == nil {
+				regenlist = append(regenlist, l.Bytes())
+				regenpath = append(regenpath, nameSegments[0])
+			} else {
+				regen(regenlist, hkid, b)
+			}
 			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
 		}
 		if typeString == "tag" {
 			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
+			regenlist = nil
+			regenpath = []string{}
 			t, err := GetTag(objecthash, nameSegments[0])
 			if !t.Verifiy() {
 				b = nil
 				err = errors.New("Tag Verifiy Failed")
+				return err
 			}
 			typeString = t.TypeString
 			objecthash = t.HashBytes
 		}
 		if typeString == "commit" {
 			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
+			regenlist = nil
+			regenpath = []string{}
 			c, err := GetCommit(objecthash)
 			if !c.Verifiy() {
 				b = nil
 				err = errors.New("Commit Verifiy Failed")
+				return err
 			}
 			l, err := GetList(c.listHash)
 			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
@@ -101,6 +120,10 @@ func post(hkid []byte, path string, b blob) (err error) {
 	}
 	b = nil
 	return
+}
+
+func regen(objectsToRegen [][]byte, hkid []byte, b blob)(error) {
+	return nil
 }
 
 func initRepo(hkid []byte, path string) (repoHkid []byte) {
