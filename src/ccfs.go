d41b050820a534682c73e7cfba27a99678e63b3a
Aaron Goldman <GoldmanAaron@gmail.com>
2013-06-13 09:48:35 -0400

    split out get and put functions to their own files

--- src/ccfs.go
+++ src/ccfs.go
@@ -3,10 +3,9 @@ package main
 import (
 	"bufio"
 	golist "container/list"
-	"errors"
+
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
@@ -22,106 +21,6 @@ func main() {
 }
 
 type hkid []byte
-
-func get(objecthash hkid, path string) (b blob, err error) {
-	typeString := "commit"
-	//objecthash := hkid
-	err = nil
-	nameSegments := strings.SplitN(path, "/", 2)
-	for {
-		switch typeString {
-		case "blob":
-			if len(nameSegments) < 2 {
-				b, err = GetBlob(objecthash)
-				return
-			}
-		case "list":
-			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
-			l, _ := GetList(objecthash)
-			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
-		case "tag":
-			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
-			t, err := GetTag(objecthash, nameSegments[0])
-			if !t.Verifiy() {
-				b = nil
-				err = errors.New("Tag Verifiy Failed")
-				return b, err
-			}
-			typeString = t.TypeString
-			objecthash = t.HashBytes
-		case "commit":
-			c, err := GetCommit(objecthash)
-			if !c.Verifiy() {
-				b = nil
-				err = errors.New("Commit Verifiy Failed")
-				return b, err
-			}
-			l, err := GetList(c.listHash)
-			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
-		}
-		if err != nil {
-			return nil, err
-		}
-	}
-	b = nil
-	return
-}
-
-func post(objecthash hkid, path string, b blob) (err error) {
-	typeString := "commit"
-	//objecthash := hkid
-	err = nil
-	nameSegments := strings.SplitN(path, "/", 2)
-	regenlist := golist.New()
-	regenpath := []string{}
-	for {
-		switch typeString {
-		case "blob":
-			if len(nameSegments) < 2 {
-				b, err = GetBlob(objecthash)
-				return
-			}
-		case "list":
-			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
-			l, _ := GetList(objecthash)
-			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
-			if err == nil {
-				regenlist.PushBack(l)
-				regenpath = append(regenpath, nameSegments[0])
-			} else {
-				regen(regenlist, objecthash, b)
-			}
-		case "tag":
-			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
-			t, _ := GetTag(objecthash, nameSegments[0])
-			if !t.Verifiy() {
-				return errors.New("Tag Verifiy Failed")
-			}
-			typeString = t.TypeString
-			objecthash = t.HashBytes
-			regenlist.Init()
-			regenlist.PushBack(t)
-		case "commit":
-			c, _ := GetCommit(objecthash)
-			if !c.Verifiy() {
-				return errors.New("Commit Verifiy Failed")
-			}
-			regenlist.Init()
-			regenlist.PushBack(c)
-			l, _ := GetList(c.listHash)
-			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
-		}
-		if err != nil {
-			return err
-		}
-		if typeString != "list" {
-
-			regenpath = []string{}
-		}
-	}
-	b = nil
-	return
-}
 
 func regen(objectsToRegen *golist.List, objecthash hkid, b blob) error {
 	return nil
---
+++ src/get.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"strings"
+)
+
+func get(objecthash hkid, path string) (b blob, err error) {
+	typeString := "commit"
+	//objecthash := hkid
+	err = nil
+	nameSegments := strings.SplitN(path, "/", 2)
+	for {
+		switch typeString {
+		case "blob":
+			if len(nameSegments) < 2 {
+				b, err = GetBlob(objecthash)
+				return
+			}
+		case "list":
+			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
+			l, _ := GetList(objecthash)
+			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
+		case "tag":
+			nameSegments = strings.SplitN(nameSegments[1], "/", 2)
+			t, err := GetTag(objecthash, nameSegments[0])
+			if !t.Verifiy() {
+				b = nil
+				err = errors.New("Tag Verifiy Failed")
+				return b, err
+			}
+			typeString = t.TypeString
+			objecthash = t.HashBytes
+		case "commit":
+			c, err := GetCommit(objecthash)
+			if !c.Verifiy() {
+				b = nil
+				err = errors.New("Commit Verifiy Failed")
+				return b, err
+			}
+			l, err := GetList(c.listHash)
+			typeString, objecthash = l.hash_for_namesegment(nameSegments[0])
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	b = nil
+	return
+}
