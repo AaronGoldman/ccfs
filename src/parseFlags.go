68af4179ed0c6843593cef0f215fd92b03f41254
tsaekao3 <tsaekao3@gatech.edu>
2013-10-31 17:40:48 -0400

    Used command arguments to make calls to CCFS core
    Created stub function, fileSystemPath2CollectionPath, to get hkid and
    collectionPath from the fileSystemPath. Prints out error.

--- src/parseFlags.go
+++ src/parseFlags.go
@@ -20,37 +20,73 @@ func parseFlags() (*string, *string, bool) {
 	return action, path, flagged
 }
 
-func takeActions(action *string, path *string) {
+func fileSystemPath2CollectionPath(fileSystemPath *string) (h HKID, collectionPath string) {
+
+	return HKID{}, ""
+}
+
+func takeActions(action *string, fileSystemPath *string) {
 	if *action != "" {
 		log.Println("action", *action)
 	}
-	if *path != "/" {
-		log.Println("path", *path)
+	if *fileSystemPath != "/" {
+		log.Println("path", *fileSystemPath)
 	}
 
 	in := bufio.NewReader(os.Stdin)
+	var err error
+	h, collectionPath := fileSystemPath2CollectionPath(fileSystemPath)
 	switch *action {
 	case "createDomain":
 		fmt.Println("Type in domain name:")
 		domainName, _ := in.ReadString('\n')
 		fmt.Printf("Name of Domain: %s", domainName)
+		err = InitDomain(h, fmt.Sprintf("%s/%s", collectionPath, domainName))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
 	case "createRepository":
 		fmt.Println("Type in repository name:")
 		repositoryName, _ := in.ReadString('\n')
 		fmt.Printf("Name of Repository: %s", repositoryName)
+		err = InitRepo(h, fmt.Sprintf("%s/%s", collectionPath, repositoryName))
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	case "insertDomain":
 		fmt.Println("Type in domain name:")
 		domainName, _ := in.ReadString('\n')
 		fmt.Println("Insert HKID as a hexadecimal number:")
-		hkid, _ := in.ReadString('\n')
+		hex, _ := in.ReadString('\n')
+		foreign_hkid, err := HkidFromHex(hex)
+		if err != nil {
+			log.Println(err)
+		}
 		fmt.Printf("Name of Domain: %s", domainName)
-		fmt.Printf("hkid: %s", hkid)
+		fmt.Printf("hkid: %s", h)
+		err = InsertDomain(h, fmt.Sprintf("%s/%s", collectionPath, domainName), foreign_hkid)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	case "insertRepository":
 		fmt.Println("Type in repository name:")
 		repositoryName, _ := in.ReadString('\n')
 		fmt.Println("Insert HKID as a hexadecimal number:")
-		hkid, _ := in.ReadString('\n')
+		hex, _ := in.ReadString('\n')
+		foreign_hkid, err := HkidFromHex(hex)
+		if err != nil {
+			log.Println(err)
+		}
 		fmt.Printf("Name of Repository: %s", repositoryName)
-		fmt.Printf("hkid: %s", hkid)
+		fmt.Printf("hkid: %s", h)
+		err = InsertRepo(h, fmt.Sprintf("%s/%s", collectionPath, repositoryName), foreign_hkid)
+		if err != nil {
+			log.Println(err)
+			return
+		}
 	}
 }
