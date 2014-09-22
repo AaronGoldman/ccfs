6d7e8ec85956bd714c6d81c3070829be221eaaf2
Chris Beaulieu <cbeaulieu6@gatech.edu>
2014-09-22 15:05:55 -0400

    Made test for File System
    Test creates file, then writes to file, then reads from file, and finally deletes the file. If any of these fail the test returns with the error.

--- interfaces/fuse/fuse_test.go
+++ interfaces/fuse/fuse_test.go
@@ -2,7 +2,10 @@
 package fuse
 
 import (
+	"bytes"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -11,4 +14,51 @@ const mountpiont = "../../mountpiont"
 func TestPwd(t *testing.T) {
 	fileInfos, _ := ioutil.ReadDir(mountpiont)
 	t.Logf("pwd: %s", fileInfos)
+}
+
+func TestFileFunctions(t *testing.T) {
+
+	const testFile = "textfile.txt"
+
+	fileInfos, err := ioutil.ReadDir(mountpoint)
+	if err != nil {
+		t.Errorf("Testing Directory Existence Error - %s /n", err)
+	}
+	for fileInfo := range fileInfos {
+		if fileInfo == testFile {
+			t.ErrorF("File Creation Error - File Already Exists")
+			return
+		}
+	}
+	path := filepath.Join(mountpoint, testfile)
+	ioutil.WriteFile(path, []byte(""), 0777)
+	fileFound := false
+	for fileInfo := range fileInfos {
+		if fileInfo == testFile {
+			fileFound = true
+		}
+	}
+	if fileFound != true {
+		t.Errorf("File Creation Error - Creation Failed")
+		return
+	}
+
+	inBytes = "Test Data"
+	ioutil.WriteFile(path, []byte(inOutBytes), 0777)
+	outBytes, err = ioutil.ReadFile(path, []byte, 0777)
+	if err != nil {
+		t.errorf("Read File Error - %s", err)
+		return
+	}
+	if !bytes.Equal(inBytes, outBytes) {
+		t.errorf("Data Write Error - Data Read Not Equal to Data Written")
+		return
+	}
+
+	err = os.Remove(path)
+	if err != nil {
+		t.errorf("File Deletion Error - %s", err)
+		return
+	}
+
 }
