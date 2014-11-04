1854418ea346b2847b0c67d3f1ef3f04e2ee722c
Aaron Goldman <GoldmanAaron@gmail.com>
2014-11-04 09:56:01 -0500

    commandLineInterface renamed to repl and placed in own file

--- ccfs.go
+++ ccfs.go
@@ -3,11 +3,7 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
-	"os"
-	"time"
 )
 
 func init() {
@@ -17,61 +13,6 @@ func init() {
 func main() {
 	log.SetFlags(log.Lshortfile)
 	start()
-	commandLineInterface()
-	return
-}
-
-var continueCLI = true
-
-func commandLineInterface() {
-	in := bufio.NewReader(os.Stdin)
-	ch := make(chan string, 1)
-	for continueCLI {
-		fmt.Printf("CCFS:$ ")
-		go func() {
-			line, err := in.ReadString('\n')
-			if err != nil {
-				fmt.Printf("[CLI] %s", err)
-			} else {
-				ch <- line
-			}
-		}()
-	label:
-		for continueCLI {
-			select {
-			case line := <-ch:
-				switch line {
-				case "quit\n":
-					continueCLI = false
-				case "createDomain\n":
-					fmt.Printf("Usage: createDomain Path\n")
-				case "createRepository\n":
-					fmt.Printf("Usage: createRepository Path\n")
-				case "insertDomain\n":
-					// ID Path HKID (Hex)
-					fmt.Printf("Usage: insertDomain Path HKID(Hex)\n")
-				case "insertRepository\n":
-					// IR Path HKID (Hex)
-					fmt.Printf("Usage: insertRepository Path HKID(Hex)\n")
-				case "insertKey\n":
-					// Should print out HKID of the new key
-					fmt.Printf("Usage: insertKey key(HEX)\n")
-				case "status\n":
-					// This prints out the status of the services
-					fmt.Printf("Usage: status prints the status page\n")
-				default:
-					fmt.Printf(`Type quit to quit
-createDomain Creates a new domain at path
-createRepository Creates a new repository at path
-insertDomain Inserts the domain HKID at path
-insertRepository Inserts the repository HKID at path
-`)
-				}
-				break label
-			case <-time.After(time.Millisecond * 250):
-			}
-
-		}
-	}
+	repl()
 	return
 }
