c1ec63b838fc5dfed0edb3c1cb7687bb49749e47
Mallika <mallika.lc@gmail.com>
2015-03-01 20:02:31 -0500

    Prompt user to add themselves to fuse user group. Can build/run CCFS. need to test on new user.

--- interfaces/fuse/fuseIntr.go
+++ interfaces/fuse/fuseIntr.go
@@ -5,22 +5,23 @@ package fuse
 import (
 	"log"
 	"os"
-
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
-	//"bazil.org/fuse/fs/fstestutil"
+	//"bazil.org/fuse/fs/fstestutil" 
 )
 
 var running bool
 
 //Start mounts the local seed on the local file system
+//fuse group id = 104
 func Start() {
 	//fstestutil.DebugByDefault()
-	go startFSintegration()
+	go checkGroup()
+	go startFSintegration() 
 	running = true
 }
 
-func ccfsUnmount(mountpoint string) {
+func ccfsUnmount(mountpoint string) { 
 	err := fuse.Unmount(mountpoint)
 	if err != nil {
 		log.Printf("Could not unmount: %s", err)
@@ -31,4 +32,20 @@ func ccfsUnmount(mountpoint string) {
 
 func generateInode(NodeID fuse.NodeID, name string) fuse.NodeID {
 	return fuse.NodeID(fs.GenerateDynamicInode(uint64(NodeID), name))
+}
+
+func checkGroup() { 
+	groups, _ := os.Getgroups()
+	var fuseGroup bool
+	fuseGroup = false
+	for i := 0; i < len(groups); i++ {
+		if (groups[i]==104) {
+			fuseGroup = true
+		}
+	}
+	if fuseGroup==false {
+		log.Printf("Add yourself to the fuse usergroup by:\n useradd -G fuse [username]")
+	}
+	
+	
 }
